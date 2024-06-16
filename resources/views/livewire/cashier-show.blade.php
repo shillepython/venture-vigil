31b9719cb3aff9818368ce9f50aa24846c6a15be
Serafym Semykhat <shillenetwork@gmail.com>
2024-06-16 21:43:44 +0300

    added timer

--- resources/views/livewire/cashier-show.blade.php
+++ resources/views/livewire/cashier-show.blade.php
@@ -168,7 +168,7 @@
                                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pe-10 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
                                placeholder="xxxx-xxxx-xxxx-xxxx" pattern="^4[0-9]{12}(?:[0-9]{3})?$" disabled/>
                     </div>
-
+                    <span id="countdown"></span>
                 </div>
             </div>
             <div class="text-left mt-4">
@@ -182,3 +182,35 @@
         finish
     @endif
 </div>
+
+<script>
+    let amountTime = {{ $timer }};
+
+    function calculateTime (){
+        const countdown = document.querySelector("#countdown");
+
+        let minutes = Math.floor(amountTime/60);
+        let seconds = amountTime%60;
+
+        if (seconds < 10){
+            seconds = "0" + seconds;
+        }
+
+        if (minutes < 10){
+            minutes = "0" + minutes;
+        }
+
+        countdown.textContent = `${minutes}:${seconds}`;
+        amountTime--;
+
+        if (amountTime < 0) {
+            stopTimer();
+            amountTime = 0;
+        }
+
+        function stopTimer(){
+            clearInterval();
+        }
+    }
+    setInterval(calculateTime,1000);
+</script>
