a77bd098e859025317ffc87d918fa4edda981a59
Serafym Semykhat <shillenetwork@gmail.com>
2024-06-16 17:37:43 +0300

    change order stage

--- app/Livewire/CashierShow.php
+++ app/Livewire/CashierShow.php
@@ -8,11 +8,15 @@ class CashierShow extends Component
 {
     public $id;
     public $cashier;
+    public $minFiatUsdMessage;
+    public $enableSumbitStageOne = true;
     public $fiatUsd = 100;
+    public $fiatRub;
 
     public function mount()
     {
         $this->cashier = \App\Models\Cashier::find($this->id) ?? '';
+        $this->fiatRub = round(floatval($this->cashier->price_per_dollar) * intval($this->fiatUsd), 2);
     }
 
     public function createOrder()
@@ -20,9 +24,37 @@ class CashierShow extends Component
 
     }
 
-    public function setUsd($fiat): void
+    public function updatedFiatUsd()
+    {
+        $this->minFiatUsdMessage = '';
+        $this->enableSumbitStageOne = true;
+        if ($this->fiatUsd < 100) {
+            $this->minFiatUsdMessage = 'USD purchase amount must be at least 100';
+            $this->enableSumbitStageOne = false;
+            return;
+        }
+        $this->fiatRub = round(floatval($this->cashier->price_per_dollar) * intval($this->fiatUsd), 2);
+    }
+
+    public function updatedFiatRub()
+    {
+        $this->minFiatUsdMessage = '';
+        $this->enableSumbitStageOne = true;
+        if ($this->fiatRub < 1) {
+            $this->fiatRub = 1;
+        }
+
+        $this->fiatUsd = round(intval($this->fiatRub) / floatval($this->cashier->price_per_dollar), 2);
+        if ($this->fiatUsd < 100) {
+            $this->minFiatUsdMessage = 'USD purchase amount must be at least 100';
+            $this->enableSumbitStageOne = false;
+        }
+    }
+
+    public function setUsd($fiat)
     {
         $this->fiatUsd = $fiat;
+        $this->updatedFiatUsd();
     }
 
     public function render()
--- resources/views/livewire/cashier-show.blade.php
+++ resources/views/livewire/cashier-show.blade.php
@@ -1,12 +1,12 @@
 <div class="bg-white dark:bg-gray-800 rounded-lg shadow-xl p-12">
     <h1 class="text-2xl font-bold text-white">RUB to USD</h1>
-    <span class="text-gray-400 ">Convert Russian Rubles to United States Dollar</span>
+    <span class="text-gray-400">Convert Russian Rubles to United States Dollar</span>
     <form class="mx-auto p-6 mb-4" wire:submit="createOrder">
         <div class="mb-6">
             <div class="flex flex-row space-x-4 mb-8">
                 <div>
                     <label for="sell-currency" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Currency</label>
-                    <select wire:model="currency" id="sell-currency" name="sell-currency"
+                    <select id="sell-currency" name="sell-currency"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required disabled>
                         <option selected value="usd">USD</option>
@@ -18,7 +18,7 @@
                     <label for="sell-amount" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">You
                         want to buy
                         $</label>
-                    <input wire:model="fiatUsd" type="text" id="sell-amount" name="sell-amount"
+                    <input wire:model.live.debounce.1000ms="fiatUsd" type="text" id="sell-amount" name="sell-amount"
                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                            placeholder="200" required/>
                     <div
@@ -30,13 +30,14 @@
                         <span wire:click="setUsd(300)"
                               class="bg-gray-100 px-3 py-1 rounded dark:bg-gray-700 dark:hover:bg-green-500 transition-colors cursor-pointer">300</span>
                     </div>
+                    <span class="text-sm text-red-800">{{ $minFiatUsdMessage }}</span>
                 </div>
 
             </div>
             <div class="flex flex-row space-x-4">
                 <div>
                     <label for="sell-currency" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Currency</label>
-                    <select wire:model="currency" id="sell-currency" name="sell-currency"
+                    <select id="sell-currency" name="sell-currency"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required disabled>
                         <option value="usd">USD</option>
@@ -46,7 +47,7 @@
                 </div>
                 <div>
                     <label for="sell-amount" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Will cost</label>
-                    <input wire:model="fiatUsd" type="text" id="sell-amount" name="sell-amount"
+                    <input wire:model.live="fiatRub" type="text" id="sell-amount" name="sell-amount"
                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                            placeholder="200" required/>
                 </div>
@@ -55,8 +56,8 @@
             <span
                 class="block mt-4 text-sm text-gray-500 dark:text-gray-300">*Price per dollar{{ $cashier->price_per_dollar }}</span>
         </div>
-        <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full sm:w-auto text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition duration-150 ease-in-out">
+        <button type="submit" {{ $enableSumbitStageOne ? '' : 'disabled' }}
+                class="text-white {{ $enableSumbitStageOne ? 'dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' : 'dark:bg-gray-600' }} bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full sm:w-auto text-center transition duration-150 ease-in-out">
             Submit
         </button>
     </form>
