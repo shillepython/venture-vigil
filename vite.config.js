509824e322d68939fac3a4e1a4e4a4da849b39ed
nktvit <nikita.vitkovsky@icloud.com>
2024-06-26 07:55:47 +0100

    ✏️Fix prod scripts

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/js/app.js',
                 'resources/react/index.jsx'
             ],
             refresh: true,
