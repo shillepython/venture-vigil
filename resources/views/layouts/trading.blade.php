41acdc26c547ec7479b72d4888af07fd97a8cd46
Serafym Semykhat <shillenetwork@gmail.com>
2024-08-27 11:49:36 +0300

    added logs

--- resources/views/layouts/trading.blade.php
+++ resources/views/layouts/trading.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="google-site-verification" content="9olAR0iGm3JZXhSKgL49tQw_UPrq0FUYm8m0IKonZj0" />
-        
+
         <title>{{ config('app.name', 'Venture Vigil') }}</title>
 
         <!-- Fonts -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <meta name="kadam-verification" content="kadam92f488dcd397d7661609116020ae70cd" />
+    <meta name="google-site-verification" content="9olAR0iGm3JZXhSKgL49tQw_UPrq0FUYm8m0IKonZj0" />
+    
     @viteReactRefresh
     @vite(['resources/react/index.jsx', 'resources/css/app.css'])
 
